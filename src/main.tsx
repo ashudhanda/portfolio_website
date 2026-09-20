3f7429ffb8643fa91493c3d120447bc4b4cf295d
Ashu <ashudhanda33@gmail.com>
2026-09-21 01:26:28 +0530

    docs: comment the React entry point mounting the #root element

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+// App entry point: mounts the React component tree into the
+// <div id="root"> element declared in index.html.
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
