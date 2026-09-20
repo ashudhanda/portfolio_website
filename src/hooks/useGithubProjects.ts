a2658292ac78b83dfd1e489a09e04e448de3b68a
Ashu <ashudhanda33@gmail.com>
2026-09-21 01:26:17 +0530

    chore: remove unused useMemo import from useGithubProjects

--- src/hooks/useGithubProjects.ts
+++ src/hooks/useGithubProjects.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { GithubRepo } from '@/types';
 
 // Whitelist of repos to display
